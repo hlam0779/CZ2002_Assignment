88f075c5245b2f0b8c19479f73209229cf20138b
hlam0779 <41835395+hlam0779@users.noreply.github.com>
2018-10-30 00:49:54 +0800

    Update Lecture.java

--- Lecture.java
+++ Lecture.java
@@ -2,6 +2,10 @@ import java.util.HashSet;
 import java.io.Serializable;
 
 public class Lecture implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5281074533738230052L;
 	private int lecId;
 	private int capacity;
 	private HashSet<Student> students;
@@ -20,17 +24,26 @@ public class Lecture implements Serializable{
 	
 	public HashSet<Student> getStudents() { return students; }
 	
+	public int currentOccupied() { return this.students.size(); }
+	
+	/*Add a student to the student list of this lecture
+	 * Return true if successfully add the student to this course and false otherwise
+	 */
 	public boolean addStudent(Student s) {
+		
+		//Check vacancies
 		if (capacity > currentOccupied()) {
 			this.students.add(s);
-			System.out.println("Successfully register this student for this lecture");
+			System.out.println("Successfully register this student for this lecture\n");
 			return true;
 		}
-		System.out.println("No available vacancies for this lecture");
+		System.out.println("No available vacancies for this lecture\n");
 		return false;
 	}
 	
-	public int currentOccupied() {
-		return this.students.size();
+	//This is used for printing the vacancies of this lecture in the checkVacancies method of the ManageApp
+	@Override
+	public String toString() {
+		return "This lecture has "+ (capacity-currentOccupied())+ " vacancies\n";
 	}
 }
