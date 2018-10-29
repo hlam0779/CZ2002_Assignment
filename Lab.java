892d948b290d758aa6fbe1ed4f6dd01e6fdbc8bc
hlam0779 <41835395+hlam0779@users.noreply.github.com>
2018-10-30 00:49:32 +0800

    Update Lab.java

--- Lab.java
+++ Lab.java
@@ -2,6 +2,10 @@ import java.util.HashSet;
 import java.io.Serializable;
 
 public class Lab implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -9019537250606693375L;
 	private int labId;
 	private HashSet<Student> students;
 	private int capacity;
@@ -20,15 +24,26 @@ public class Lab implements Serializable{
 	
 	public HashSet<Student> getStudents() { return students; }
 	
+	public int currentOccupied() { return this.students.size(); }
+	
+	/*Add a student to the student list of this lab session 
+	 * Return true if successfully add the student to this course and false otherwise
+	 */
 	public boolean addStudent(Student s) {
+		
+		//Check vacancies
 		if (capacity > currentOccupied()) {
 			this.students.add(s);
-			System.out.println("Successfully register this student for this lab session");
+			System.out.println("Successfully register this student for this lab session\n");
 			return true;
 		}
-		System.out.println("No available vacancies for this lab session");
+		System.out.println("No available vacancies for this lab session\n");
 		return false;
 	}
 	
-	public int currentOccupied() { return this.students.size(); }
+	//This is used for printing the vacancies of this lab session in the checkVacancies method of the ManageApp
+	@Override
+	public String toString() {
+		return "This lab has "+ (capacity-currentOccupied())+ " vacancies\n";
+	}
 }
